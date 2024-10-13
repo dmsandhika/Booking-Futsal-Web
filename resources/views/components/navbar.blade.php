da7621f5977ba5e263f71da9ae8104c913e41912
Dimas Andhika Firmansyah <evandimas818@gmail.com>
2024-10-13 21:28:04 +0700

    cek riwayat transaksi & tolak transaksi jika dijam tersebut sudah ada bookingan

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,6 +5,7 @@
 	<div class="navbar-end">
 		<a href="/" class="navbar-item ">Home</a>
 		<a href="/booking" class="navbar-item">Booking</a>
+		<a href="/search" class="navbar-item">Cari</a>
 		<a class="navbar-item">Contact</a>
 	</div>
 </div>
